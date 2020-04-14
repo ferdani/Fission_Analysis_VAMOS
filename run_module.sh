f1562626f6360ce1cc474be4707add050dca0944
Dani <danisheldoncooper@gmail.com>
2020-04-14 19:28:38 +0200

    Ready to launch the first module package

--- run_module.sh
+++ run_module.sh
@@ -7,3 +7,8 @@ dani.fernandez@usc.es
 
 The control area to run an independent module
 '
+
+####################################### Modules List ###################################################
+
+#----------------------- 0 - Module - DCs_Calibration --------------------------------------------------
+python -i -c 'from Modules import Module_selector' 'DCs_Calibration'
