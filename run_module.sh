657f620dc6d669093450e01b1f32b1e12d99bac8
Dani <danisheldoncooper@gmail.com>
2020-04-16 19:45:12 +0200

    Adding a testing module for test new things, first step to build sample-generic module

--- run_module.sh
+++ run_module.sh
@@ -5,10 +5,22 @@ Created on Tue Apr 14 17:30:00 2020
 @author: Daniel Fernandez Fernandez
 dani.fernandez@usc.es
 
-The control area to run an independent module
+The control area to run an independent module:
+0 --> Testing
+1 --> DCs_Calibration
+2 --> Phi_Acceptance
+3 --> dE_vs_E
 '
 
-####################################### Modules List ###################################################
+####################################### Modules Command List ###################################################
+#----------------------- 0 - Module - Testing --------------------------------------------------
+python -i -c 'from Modules import Module_selector' 'Testing'
 
-#----------------------- 0 - Module - DCs_Calibration --------------------------------------------------
-python -i -c 'from Modules import Module_selector' 'DCs_Calibration'
+#----------------------- 1 - Module - DCs_Calibration --------------------------------------------------
+#python -i -c 'from Modules import Module_selector' 'DCs_Calibration'
+
+#----------------------- 2 - Module - Phi_Acceptance --------------------------------------------------
+#python -i -c 'from Modules import Module_selector' 'Phi_Acceptance'
+
+#----------------------- 3 - Module - dE_vs_E --------------------------------------------------
+#python -i -c 'from Modules import Module_selector' 'dE_vs_E'
