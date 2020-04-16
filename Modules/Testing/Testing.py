0eda8727bf0de5fda4f4d724b71aeddd6ad3fa65
Dani <danisheldoncooper@gmail.com>
2020-04-16 19:53:00 +0200

    Add Testing module to assay and test new things in a sample-generic module

---
+++ Modules/Testing/Testing.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 15 15:46:00 2020
+
+@author: Daniel Fernandez Fernandez
+dani.fernandez@usc.es
+
+TESTING MODULE
+"""
+
+''' importanciones ''''
+import numpy as np
+from Framework import
+
+
+
+
+
+
+''' Abrir y leer fichero '''
+
+#coger ficheros del Data_hdf5 folder que han salido del converter root2hdf5 en framework y ue han sido
+#convertidos de los RootA una vez hechas las calibraciones
+
+
+
+
+
+''' Do some analysis here ''''
+
+
+
+
+
+
+
+
+''' Plotear variables '''
+
+
+
+
+
+
+''' Salvar si acaso datos'''
+
+
+
+import numpy as np
+
+A = np.array([])
+
+print('1 Dcs')
+print(A)
+
+
+if __name__ == "__main__":
+    print('2 Dcs')
