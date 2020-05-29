9ebf37a6383e8240328aec4583e687b0f572b685
Dani <danisheldoncooper@gmail.com>
2020-05-29 12:20:38 +0200

    first step in ICs_Calibration module

---
+++ Modules/ICs_Calibration/ICs_Calibration.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 29 12:13:00 2020
+
+@author: Daniel Fernandez Fernandez
+dani.fernandez@usc.es
+
+ICs_Calibration MODULE -- The Ionization Chambers Calibration Module
+
+The code generates:
+-- Outputfiles/Figures/
+
+"""
+'''
+----------------------------------------------------------------- Protected part ----------------------------------------------------------------------------------------------------------------
+'''
+import os, sys
+sys.path.append('.')
+sys.path.append('..')
+basepath = os.path.abspath(__file__).rsplit('/Fission_Analysis_VAMOS/',1)[0]+'/Fission_Analysis_VAMOS/'
+sys.path.append(basepath)
+Module_path = basepath + '/Modules/ICs_Calibration/'
+sys.path.append(Module_path)
+
+'''
+---------------------------------------------------- Import packages and Framework functions ----------------------------------------------------------------------------------------------------
+'''
+import numpy as np
+import root_numpy as rn
+import Framework.read_and_save.read_and_save as RAS
+from Plotter.Plotter import Plotter
