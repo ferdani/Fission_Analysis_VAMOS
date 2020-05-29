5c58d86dac4d57997157c8a29ac20cae423d837a
Dani <danisheldoncooper@gmail.com>
2020-05-29 17:02:13 +0200

    added lines to convert files for Charge_states module

--- Framework/converter_root2hdf5/run_converter_root2hdf5.sh
+++ Framework/converter_root2hdf5/run_converter_root2hdf5.sh
@@ -15,6 +15,11 @@ From here one can run the converter_root2hdf5.py to converts root in hdf5 files 
 #python -i -c 'import converter_root2hdf5' -s 'TEST' -n 'Analysis_TEST_file' -b 'Z,Zi,Q,Qi,M,Mr,M_Q,E,dE,V,Gamma,Beta,Brho,Xf,Yf,Delta,TP_PhiL,TP_ThetaL' -p 'Z > 0.0 && Zi > 0.0 && TP_ThetaL > -1500.0 && Gamma > 0.0 && Brho > 0.0 && Beta > 0.0 && E > 0.0 && dE > 0.0 && Xf > -1500.0 && Yf > -1500.0 && M > 0.0 && M_Q > 0.0 && Mr > 0.0 && Q > 0.0 && Qi > 0.0 && TP_PhiL > -1500.0 && Delta > 0.0 && V > 0.0'
 
 ###################################### Files for DCs_X_Y_Theta_Phi_FP MODULE ###########################
-python -c 'import converter_root2hdf5' -s '14' -n 'Analysis_14_file_DC_variables' -p 'Z > 0.0 && Zi > 0.0 && M > 0.0' -b 'Xf,Yf,Tf,Pf,PhiL,ThetaL'
-python -c 'import converter_root2hdf5' -s '21' -n 'Analysis_21_file_DC_variables' -p 'Z > 0.0 && Zi > 0.0 && M > 0.0' -b 'Xf,Yf,Tf,Pf,PhiL,ThetaL'
-python -c 'import converter_root2hdf5' -s '14+21' -n 'Analysis_14+21_file_DC_variables' -p 'Z > 0.0 && Zi > 0.0 && M > 0.0' -b 'Xf,Yf,Tf,Pf,PhiL,ThetaL'
+#python -c 'import converter_root2hdf5' -s '14' -n 'Analysis_14_file_DC_variables' -p 'Z > 0.0 && Zi > 0.0 && M > 0.0' -b 'Xf,Yf,Tf,Pf,PhiL,ThetaL'
+#python -c 'import converter_root2hdf5' -s '21' -n 'Analysis_21_file_DC_variables' -p 'Z > 0.0 && Zi > 0.0 && M > 0.0' -b 'Xf,Yf,Tf,Pf,PhiL,ThetaL'
+#python -c 'import converter_root2hdf5' -s '14+21' -n 'Analysis_14+21_file_DC_variables' -p 'Z > 0.0 && Zi > 0.0 && M > 0.0' -b 'Xf,Yf,Tf,Pf,PhiL,ThetaL'
+
+###################################### Files for Charge_states MODULE ###########################
+python -c 'import converter_root2hdf5' -s '14' -n 'Analysis_14_file_ChargeStates_variables' -p 'Z > 0.0 && Zi > 0.0 && M > 0.0' -b 'Z,Zi,M,M_Q'
+python -c 'import converter_root2hdf5' -s '21' -n 'Analysis_21_file_ChargeStates_variables' -p 'Z > 0.0 && Zi > 0.0 && M > 0.0' -b 'Z,Zi,M,M_Q'
+python -c 'import converter_root2hdf5' -s '14+21' -n 'Analysis_14+21_file_ChargeStates_variables' -p 'Z > 0.0 && Zi > 0.0 && M > 0.0' -b 'Z,Zi,M,M_Q'
