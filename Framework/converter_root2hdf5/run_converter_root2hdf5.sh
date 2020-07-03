34802970293b080cf5b992c282bec66447b89e4b
Dani <danisheldoncooper@gmail.com>
2020-07-03 11:04:41 +0200

    for ICs_Calibration data

--- Framework/converter_root2hdf5/run_converter_root2hdf5.sh
+++ Framework/converter_root2hdf5/run_converter_root2hdf5.sh
@@ -25,6 +25,11 @@ From here one can run the converter_root2hdf5.py to converts root in hdf5 files 
 #python -c 'import converter_root2hdf5' -s '14+21' -n 'Analysis_14+21_file_ChargeStates_variables' -p 'Z > 0.0 && Zi > 0.0 && M > 0.0' -b 'Z,Zi,M,M_Q,Mri,Mr,Q,Qi,Xf,Yf,Tf,Pf'
 
 ###################################### Files for ToF_correction_in_AoverQ MODULE ###########################
-python -c 'import converter_root2hdf5' -s '14' -n 'Analysis_14_file_ToF_correction_in_AoverQ_variables' -p 'Z > 0.0 && Zi > 0.0 && M > 0.0' -b 'Z,Zi,M,M_Q,Mri,Mr,Q,Qi,Xf,Yf,Tf,Pf,MW_Nr,Brho,T,D'
-python -c 'import converter_root2hdf5' -s '21' -n 'Analysis_21_file_ToF_correction_in_AoverQ_variables' -p 'Z > 0.0 && Zi > 0.0 && M > 0.0' -b 'Z,Zi,M,M_Q,Mri,Mr,Q,Qi,Xf,Yf,Tf,Pf,MW_Nr,Brho,T,D'
-python -c 'import converter_root2hdf5' -s '14+21' -n 'Analysis_14+21_file_ToF_correction_in_AoverQ_variables' -p 'Z > 0.0 && Zi > 0.0 && M > 0.0' -b 'Z,Zi,M,M_Q,Mri,Mr,Q,Qi,Xf,Yf,Tf,Pf,MW_Nr,Brho,T,D'
+#python -c 'import converter_root2hdf5' -s '14' -n 'Analysis_14_file_ToF_correction_in_AoverQ_variables' -p 'Z > 0.0 && Zi > 0.0 && M > 0.0' -b 'Z,Zi,M,M_Q,Mri,Mr,Q,Qi,Xf,Yf,Tf,Pf,MW_Nr,Brho,T,D'
+#python -c 'import converter_root2hdf5' -s '21' -n 'Analysis_21_file_ToF_correction_in_AoverQ_variables' -p 'Z > 0.0 && Zi > 0.0 && M > 0.0' -b 'Z,Zi,M,M_Q,Mri,Mr,Q,Qi,Xf,Yf,Tf,Pf,MW_Nr,Brho,T,D'
+#python -c 'import converter_root2hdf5' -s '14+21' -n 'Analysis_14+21_file_ToF_correction_in_AoverQ_variables' -p 'Z > 0.0 && Zi > 0.0 && M > 0.0' -b 'Z,Zi,M,M_Q,Mri,Mr,Q,Qi,Xf,Yf,Tf,Pf,MW_Nr,Brho,T,D'
+
+###################################### Files for ICs_Calibration MODULE ###########################
+python -c 'import converter_root2hdf5' -s '14' -n 'Analysis_14_file_ICs_Calibration_variables' -p 'Z > 0.0 && Zi > 0.0 && M > 0.0' -b 'ICE,ICdE0,ICdE1,ICdE2,ICdE3,ICdE4,ICdE5,E_0,dE_0,E_1,dE_1,E_2,dE_2,E_3,dE_3,E_4,dE_4,ICdE,ICdEM1,IcdE,IcEres,IcEtot,IcdE_bragg,IcEres_bragg,E,dE'
+python -c 'import converter_root2hdf5' -s '21' -n 'Analysis_21_file_ICs_Calibration_variables' -p 'Z > 0.0 && Zi > 0.0 && M > 0.0' -b 'ICE,ICdE0,ICdE1,ICdE2,ICdE3,ICdE4,ICdE5,E_0,dE_0,E_1,dE_1,E_2,dE_2,E_3,dE_3,E_4,dE_4,ICdE,ICdEM1,IcdE,IcEres,IcEtot,IcdE_bragg,IcEres_bragg,E,dE'
+python -c 'import converter_root2hdf5' -s '14+21' -n 'Analysis_14+21_file_ICs_Calibration_variables' -p 'Z > 0.0 && Zi > 0.0 && M > 0.0' -b 'ICE,ICdE0,ICdE1,ICdE2,ICdE3,ICdE4,ICdE5,E_0,dE_0,E_1,dE_1,E_2,dE_2,E_3,dE_3,E_4,dE_4,ICdE,ICdEM1,IcdE,IcEres,IcEtot,IcdE_bragg,IcEres_bragg,E,dE'
